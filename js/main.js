a15b144d4e274338b83e75afc31788694b698930
enoquetembe <enoquetembetembe10@gmail.com>
2023-01-09 16:32:38 -0300

    refatoring

--- js/main.js
+++ js/main.js
@@ -1,84 +1,70 @@
-const pomodoroDurationInput = document.getElementById("pomodoro-duration")
-const shortBreakDurationInput = document.getElementById("short-break-duration")
-const longBreakDurationInput = document.getElementById("long-break-duration")
-const startButton = document.getElementById("button-start")
-const pauseButton = document.getElementById("button-pause")
-const stopButton = document.getElementById("button-stop")
-const minutesElement = document.getElementById("minutes")
-const secondsElement = document.getElementById("seconds")
-
-let currentDuration = 0 // The current duration in seconds
-let intervalId = null // The ID returned by setInterval
-
-// Update the timer display
-function updateTimer() {
-  // Convert seconds to minutes and seconds
-  const minutes = Math.floor(currentDuration / 60)
-  const seconds = currentDuration % 60
-
-  // Add leading zeros if necessary
-  minutesElement.innerText = `${minutes}`.padStart(2, "0")
-  secondsElement.innerText = `${seconds}`.padStart(2, "0")
-}
-
-// Decrement the timer by 1 second
-function tick() {
-  currentDuration--
-  updateTimer()
-
-  // Stop the timer if it reaches 0
-  if (currentDuration <= 0) {
-    stopTimer()
-  }
-}
-
-// Start the timer
-function startTimer() {
-  // Get the duration from the input field
-  currentDuration = 5 * 60
-  updateTimer()
-
-  // Start the interval
-  intervalId = setInterval(tick, 1000)
-
-  // Show the pause button and hide the start button
-  pauseButton.classList.remove("hide")
-  startButton.classList.add("hide")
-}
-
-// Pause the timer
-function pauseTimer() {
-  // Clear the interval
-  clearInterval(intervalId)
-
-  // Show the start button and hide the pause button
-  startButton.classList.remove("hide")
-  pauseButton.classList.add("hide")
-}
-
-
-// Stop the timer
-function stopTimer() {
-  // Clear the interval
-  clearInterval(intervalId)
-
-  // Reset the current duration to 0
-  currentDuration = 0
-  updateTimer()
-
-  // Show the start button and hide the pause button
-  startButton.classList.remove("hide")
-  pauseButton.classList.add("hide")
-}
-
-// Add event listeners to the buttons
-startButton.addEventListener("click", startTimer)
-pauseButton.addEventListener("click", pauseTimer)
-stopButton.addEventListener("click", stopTimer)
-
 const pomodoroLabel = document.querySelector(".pomodoro-label")
 const shortBreakLabel = document.querySelector(".shortBreak-label")
 const longBreakLabel = document.querySelector(".longBreak-label")
+
+const startButton = document.querySelector("#button-start")
+const pauseButton = document.querySelector("#button-pause")
+const stopButton = document.querySelector("#button-stop")
+
+const minutesElement = document.querySelector("#minutes")
+const secondsElement = document.querySelector("#seconds")
+
+let minutes = Number(minutesElement.textContent)
+let seconds = Number(secondsElement.textContent)
+let timer
+
+
+
+function countdown() {
+  
+    timer = setTimeout(() => {
+    let seconds = Number(secondsElement.textContent)
+    let minutes = Number(minutesElement.textContent)
+    
+    updateTimerDisplay(minutes, 0)
+    
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
+    
+    if (seconds <= 0) {
+      seconds = 3
+      --minutes
+    }
+    updateTimerDisplay(minutes, String(seconds - 1))
+   
+    countdown()
+  }, 1000)
+  
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesElement.textContent = String(minutes).padStart(2, "0")
+  secondsElement.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetControls() {
+  pauseButton.classList.add("hide")
+  startButton.classList.remove("hide")
+}
+
+function resetTimer() {
+  
+  const pomodoroLabelClassListContainsActive = pomodoroLabel.classList.contains("active")
+  const shortBreakLabelClassListContainsActive = shortBreakLabel.classList.contains("active")
+  const longBreakLabelClassListContainsActive = longBreakLabel.classList.contains("active")
+
+  if(pomodoroLabelClassListContainsActive) {
+    updateTimerDisplay(2, 0)
+  } else if(shortBreakLabelClassListContainsActive) {
+    updateTimerDisplay(5, 0)
+  } else if(longBreakLabelClassListContainsActive) {
+    updateTimerDisplay(10, 0)
+  }
+
+  clearTimeout(timer)
+}
 
 shortBreakLabel.addEventListener("click", () => {
   pomodoroLabel.classList.remove("active")
@@ -87,8 +73,11 @@ shortBreakLabel.addEventListener("click", () => {
 
   const minutes = 5
   const seconds = 0
-  minutesElement.innerText = `${minutes}`.padStart(2, "0")
-  secondsElement.innerText = `${seconds}`.padStart(2, "0")
+
+  updateTimerDisplay(minutes, seconds)
+
+  console.log(minutesElement, secondsElement)
+
 })
 
 longBreakLabel.addEventListener("click", () => {
@@ -98,8 +87,10 @@ longBreakLabel.addEventListener("click", () => {
 
   const minutes = 10
   const seconds = 0
-  minutesElement.innerText = `${minutes}`.padStart(2, "0")
-  secondsElement.innerText = `${seconds}`.padStart(2, "0")
+  updateTimerDisplay(minutes, seconds)
+
+  console.log(minutesElement, secondsElement)
+
 })
 
 pomodoroLabel.addEventListener("click", () => {
@@ -109,6 +100,28 @@ pomodoroLabel.addEventListener("click", () => {
 
   const minutes = 25
   const seconds = 0
-  minutesElement.innerText = `${minutes}`.padStart(2, "0")
-  secondsElement.innerText = `${seconds}`.padStart(2, "0")
+  updateTimerDisplay(minutes, seconds)
 })
+
+
+startButton.addEventListener("click", () => {
+  pauseButton.classList.remove("hide")
+  startButton.classList.add("hide")
+
+  countdown()
+})
+
+
+pauseButton.addEventListener("click", () => {
+  pauseButton.classList.add("hide")
+  startButton.classList.remove("hide")
+  clearTimeout(timer)
+})
+
+stopButton.addEventListener("click", () => {
+  resetControls()
+  resetTimer()
+
+})
+
+
